a3b4eafb47236c0080395083b1c91d71954f7e96
Heejung-KOREA <heejungs0304@gmail.com>
2024-04-05 13:02:26 +0900

    파일 위치 변경

---
+++ main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+from pydantic import BaseMode
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/", StaticFiles(directory = "static", html = True), name = "static")
